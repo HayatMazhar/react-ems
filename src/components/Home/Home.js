5ee28c2425fbbad7f22b4b78e2f3ac31578b7e86
Mazhar Hayat <mazhar1783@icloud.com>
2020-08-24 16:02:57 +0400

    slider

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
+import RCarousel from "../HtmlControls/RCarousel.jsx";
 
-const Home =()=>{
-    return (
-        <div className="home">
-                <h2 className="home__title">Mazhar Hayat</h2>
-                <h2 className="home__title">30 Years Old</h2>
-                <h2 className="home__title">Dubai, UAE</h2>
-                <h2 className="home__title">Software Engineer</h2>
-                <p className="home__description">
-                    Hello guys, I am currently working as a Fullstack developer 
-                </p>
-        </div>
-        
-        
-    )
-}
+const Home = () => {
+  return (
+    <>
+      <div className="slider">
+      <RCarousel />
+      </div>
+
+      
+    </>
+  );
+};
 
 export default Home;
